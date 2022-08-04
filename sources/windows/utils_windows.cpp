6d32a92b7fdfcec714b7b74487be3dc22b12b2ca
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-08-05 03:39:36 +0400

    Added copyright header to all source files

--- sources/windows/utils_windows.cpp
+++ sources/windows/utils_windows.cpp
@@ -1,10 +1,23 @@
+/*
+Copyright (C) 2019 SNMetamorph
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 #include "utils.h"
 
 // Windows API headers
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <Psapi.h>
-
 
 int32_t Utils::FindProcessID(const std::string &processName, size_t &processCount)
 {

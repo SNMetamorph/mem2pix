ea76231a331d19adccced836a30dd305c23d4b31
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-08-05 03:32:55 +0400

    Updated project info header

--- sources/project_info.h
+++ sources/project_info.h
@@ -1,6 +1,12 @@
 #pragma once
+#define MACRO_TO_STRING2(s)     #s
+#define MACRO_TO_STRING(s)      MACRO_TO_STRING2(s)
 
-#ifndef APP_VERSION
-// This value will be set automatically on compiling stage
-#define APP_VERSION ""
-#endif
+#define APP_TITLE_STR           "mem2pix"
+#define APP_GITHUB_LINK         "https://github.com/SNMetamorph/mem2pix"
+#define APP_BUILD_DATE          (__DATE__ " " __TIME__)
+#define APP_VERSION_MAJOR       1
+#define APP_VERSION_MINOR       2
+#define APP_VERSION_STRING      MACRO_TO_STRING(APP_VERSION_MAJOR)      \
+                                "." MACRO_TO_STRING(APP_VERSION_MINOR)  \
+                                "\0"
